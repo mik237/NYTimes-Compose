5512c948a1c9b86d5b4537cebe9351cb3506739e
muhammadibrahim <mik237@gmail.com>
2024-08-08 03:46:13 +0400

    updated splash-theme & launcher icon

--- app/src/main/java/me/ibrahim/nytimes/presentation/activities/MainActivity.kt
+++ app/src/main/java/me/ibrahim/nytimes/presentation/activities/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.parcelize.Parcelize
 import me.ibrahim.nytimes.domain.models.TopStory
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        installSplashScreen()
         setContent {
             NYTimesTheme {
 
